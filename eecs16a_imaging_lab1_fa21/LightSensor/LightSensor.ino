397e5152e4085174862f9cfb475dee15f7ba382d
Raghav <raghav.tech13@gmail.com>
2021-09-13 07:56:31 -0700

    Imaging 1 deployed!

--- eecs16a_imaging_lab1_fa21/LightSensor/LightSensor.ino
+++ eecs16a_imaging_lab1_fa21/LightSensor/LightSensor.ino
@@ -6,15 +6,21 @@
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin P6.0 (A0):
-  //int sensorValue = analogRead(A0);
+  int sensorValue = analogRead(A0);
   // print out the value you read:
-  //Serial.println(sensorValue);
+  float volt = 3.3 * sensorValue / 4096.0;
+  Serial.print(0);
+  Serial.print(" ");
+  Serial.print(3.3);
+  Serial.print(" ");
+  Serial.print(volt, 2);
+  Serial.println(" volts");
   delay(20);  // delay set to minimum for real time plotting 
 }
