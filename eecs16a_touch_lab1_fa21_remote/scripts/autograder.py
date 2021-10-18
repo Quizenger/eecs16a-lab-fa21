0e5eb2af4e858b838fab33ea5b761d5eca72ec43
Raghav <raghav.tech13@gmail.com>
2021-10-18 00:34:13 -0700

    Touch 1 deployed

--- eecs16a_touch_lab1_fa21_remote/scripts/autograder.py
+++ eecs16a_touch_lab1_fa21_remote/scripts/autograder.py
@@ -1,44 +1,121 @@
-q_ans = {"3a-1" : 0.0, 
-             "3a-2" : 3.2,
-             "3a-3" : 0.0,
-             "3a-4a" : 3.2,
-             "3a-5" : 0.0,
-             "3b-2" : 1000.0,
-             "3c-1a-soft" : 0.0,
-             "3c-1b-soft" : 3.2,
-             "3c-1c-soft" : (2.0/3.0)*3.2,
-             "3c-2a-soft" : (2.0/3.0)*3.2,
-             "3c-2b-soft" : (1.0/3.0)*3.2,
-             "3c-1a-hard" : 0.0,
-             "3c-1b-hard" : 3.3,
-             "3c-1c-hard" : (2.0/3.0)*3.3,
-             "3c-2a-hard" : (2.0/3.0)*3.3,
-             "3c-2b-hard" : (1.0/3.0)*3.3}
-    
-q_exp = {"3a-1" : "The multimeter is measuring the voltage across a single node (ground in this case), which is 0V.",
-            "3a-2" : "We're measuring across the voltage source, which is 3.2V.",
-            "3a-3" : "Since we're looking at an open circuit, we know that no current flows through R1. Thus, by Ohm's Law, the voltage drop across R1 must be 0.",
-            "3a-4a" : "As established in Step 3, there's no voltage drop across R1. Thus, the voltage at node 2 must be the same as that of the voltage source. The voltage at node 0 is the ground node, which means that the measured voltage will be Vz = V2 - V0 = 3.2V - 0V = 3.2V.",
-            "3a-4b" : "We were looking for Vx = Vy + Vz or something equivalent. Substituting the values for Vx, Vy, and Vz, you should get 3.2V = 0V + 3.2V.",
-            "3a-5" : "As you saw in a previous step, the voltage across R1 is 0. Since the resistance is nonzero, by Ohm's Law, the current through R1 must be 0A.",
-            "3b-1" : "Nodes 0 and 2 are really the same node since a wire connects them. Thus, there are only 2 distinct nodes in the circuit now.",
-            "3b-2" : "Recall that Ohm's Law states that V = IR. You should get R1 = Vy/Iy = 1000 ohms.",
-            "3b-3" : "They should certainly be the same!",
-            "3b-4" : "Once again, we have \"Vx = Vy + Vz\". Substitution yields 3.2V = 3.2V + 0V.",
-            "3c-1a-soft" : "The ground node is defined to be 0V.",
-            "3c-1b-soft" : "u1 is connected directly to the positive end of the voltage source, so it must be 3.2V.",
-            "3c-1c-soft" : "You should have gotten something different. Please check your circuit and where you're measuring.",
-            "3c-2a-soft" : "Vz = u2 - u0 = 2.13V - 0V = 2.13V",
-            "3c-2b-soft" : "Vy = u1 - u2 = 3.2V - 2.13V = 1.07V",
-            "3c-2c-soft" : "Vz = Vs * R2/(R1 + R2) = 3.2 * 2/3 = 2.13 V\nVy = Vs * R1/(R1 + R2) = 3.2 * 1/3 = 1.07 V\nThus, voltages obtained using the voltage divider formula agree with our measurements (there might be **slight** deviation due to measurement errors).",
-            "3c-1a-hard" : "The ground node is defined to be 0V.",
-            "3c-1b-hard" : "u1 is connected directly to the positive terminal of the voltage source, so it must be 3.3V.",
-            "3c-1c-hard" : "You should have gotten something different. Please check your circuit and where you're measuring.",
-            "3c-2a-hard" : "Vz = u2 - u0 = 2.2V - 0V = 2.2V",
-            "3c-2b-hard" : "Vy = u1 - u2 = 3.3V - 2.2V = 1.1V",
-            "3c-2c-hard" : "Vz = Vs * R2/(R1 + R2) = 3.3 * 2/3 = 2.2 V\nVy = Vs * R1/(R1 + R2) = 3.3 * 1/3 = 1.1 V\nThus, voltages obtained using the voltage divider formula agree with our measurements (there might be **slight** deviation due to measurement errors)."}
+q_ans = {
+    "3a-1" : 0.0, 
+    "3a-2" : 3.3,
+    "3a-3" : 0.0,
+    "3a-4a" : 3.3,
+    "3a-5" : 0.0,
+    "3b-2" : 1000.0,
+    "3c-1a-soft" : 0.0,
+    "3c-1b-soft" : 3.3,
+    "3c-1c-soft" : (2.0/3.0)*3.3,
+    "3c-2a-soft" : (2.0/3.0)*3.3,
+    "3c-2b-soft" : (1.0/3.0)*3.3,
+    "3c-1a-hard" : 0.0,
+    "3c-1b-hard" : 3.3,
+    "3c-1c-hard" : (2.0/3.0)*3.3,
+    "3c-2a-hard" : (2.0/3.0)*3.3,
+    "3c-2b-hard" : (1.0/3.0)*3.3
+}
 
-def autograde(answer=-1, question="3a-1", no_check=False, no_exp_if_cor=False):
+# {
+#   question_id: (
+#     correct_answer_explanation,
+#     wrong_answer_explanation
+#   )
+# }
+# If there is no explanation, use None
+q_exp = {
+    "3a-1" : (
+        "The multimeter is measuring the voltage across a single node (ground in this case), which is 0V.",
+        "The multimeter is measuring the voltage across a single node (ground in this case). What is the voltage at gound?"
+    ),
+    "3a-2" : (
+        "We're measuring across the voltage source, which is 3.3V.",
+        "Think about what component we're measuring across!"
+    ),
+    "3a-3" : (
+        "Since we're looking at an open circuit, we know that no current flows through R1. Thus, by Ohm's Law, the voltage drop across R1 must be 0.",
+        "Hint: this is an open circuit! Think about what current and Ohm's law."
+    ),
+    "3a-4a" : (
+        "As established in Step 3, there's no voltage drop across R1. Thus, the voltage at node 2 must be the same as that of the voltage source. The voltage at node 0 is the ground node, which means that the measured voltage will be Vz = V2 - V0 = 3.3V - 0V = 3.3V.",
+        "Look back in the previous steps - what is the voltage drop across R1? What are the voltages at the two nodes?"
+    ),
+    "3a-4b" : (
+        "We were looking for Vx = Vy + Vz or something equivalent. Substituting the values for Vx, Vy, and Vz, you should get 3.3V = 0V + 3.3V.",
+        None
+    ),
+    "3a-5" : (
+        "As you saw in a previous step, the voltage across R1 is 0. Since the resistance is nonzero, by Ohm's Law, the current through R1 must be 0A.",
+        "Think back in the previous steps. What is the voltage across R1? What does that say about its resistance and current by Ohm's Law?"
+    ),
+    "3b-1" : (
+        "Nodes 0 and 2 are really the same node since a wire connects them. Thus, there are only 2 distinct nodes in the circuit now.",
+        "Look at node 0 and node 2. Are they distinct nodes?"
+    ),
+    "3b-2" : (
+        "Recall that Ohm's Law states that V = IR. You should get R1 = Vy/Iy = 1000 ohms.",
+        "Recall that Ohm's Law states that V = IR."
+    ),
+    "3b-3" : (
+        "They should certainly be the same!",
+        None
+    ),
+    "3b-4" : (
+        "Once again, we have \"Vx = Vy + Vz\". Substitution yields 3.3V = 3.3V + 0V.",
+        None
+    ),
+    "3c-1a-soft" : (
+        "The ground node is defined to be 0V.",
+        "Hint: it's the ground node!"
+    ),
+    "3c-1b-soft" : (
+        "u1 is connected directly to the positive end of the voltage source, so it must be 3.3V.",
+        "u1 is connected directly to the positive end of the voltage source!"
+    ),
+    "3c-1c-soft" : (
+        None,
+        "You should have gotten something different. Please check your circuit and where you're measuring."
+    ),
+    "3c-2a-soft" : (
+        "Vz = u2 - u0 = 2.2V - 0V = 2.2V",
+        "Vz = u2 - u0. What are u2 and u0?"
+    ),
+    "3c-2b-soft" : (
+        "Vy = u1 - u2 = 3.3V - 2.2V = 1.1V",
+        "Vy = u1 - u2. What are u1 and u2?"
+    ),
+    "3c-2c-soft" : (
+        "Vz = Vs * R2/(R1 + R2) = 3.3 * 2/3 = 2.2 V\nVy = Vs * R1/(R1 + R2) = 3.3 * 1/3 = 1.1 V\nThus, voltages obtained using the voltage divider formula agree with our measurements (there might be **slight** deviation due to measurement errors).",
+        None
+    ),
+    "3c-1a-hard" : (
+        "The ground node is defined to be 0V.",
+        "Hint: it's the ground node!"
+    ),
+    "3c-1b-hard" : (
+        "u1 is connected directly to the positive end of the voltage source, so it must be 3.3V.",
+        "u1 is connected directly to the positive end of the voltage source!"
+    ),
+    "3c-1c-hard" : (
+        None,
+        "You should have gotten something different. Please check your circuit and where you're measuring."
+    ),
+    "3c-2a-hard" : (
+        "Vz = u2 - u0 = 2.2V - 0V = 2.2V",
+        "Vz = u2 - u0. What are u2 and u0?"
+    ),
+    "3c-2b-hard" : (
+        "Vy = u1 - u2 = 3.3V - 2.2V = 1.1V",
+        "Vy = u1 - u2. What are u1 and u2?"
+    ),
+    "3c-2c-hard" : (
+        "Vz = Vs * R2/(R1 + R2) = 3.3 * 2/3 = 2.2 V\nVy = Vs * R1/(R1 + R2) = 3.3 * 1/3 = 1.1 V\nThus, voltages obtained using the voltage divider formula agree with our measurements (there might be **slight** deviation due to measurement errors).",
+        None
+    )
+}
+
+def autograde(answer=-1, question="3a-1", no_check=False):
     tolerance_pct = 5.0 #error threshold percent
     answer = float(answer)
     correct = True
@@ -51,7 +128,9 @@ def autograde(answer=-1, question="3a-1", no_check=False, no_exp_if_cor=False):
     else:
         print("Sorry, that's not quite right.")
         correct = False
-    if no_exp_if_cor and correct:
-        pass
-    else:
-        print(q_exp[question])
+    
+    exp = q_exp[question]
+    if correct and exp[0]:
+        print(exp[0])
+    elif not correct and exp[1]:
+        print(exp[1])
