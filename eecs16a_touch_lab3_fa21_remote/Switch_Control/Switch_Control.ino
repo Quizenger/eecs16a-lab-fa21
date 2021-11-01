ade686c7ea174d3ab290a0be07b4b56759c17170
Raghav <raghav.tech13@gmail.com>
2021-11-01 00:31:51 -0700

    Touch 3 Deployed

--- eecs16a_touch_lab3_fa21_remote/Switch_Control/Switch_Control.ino
+++ eecs16a_touch_lab3_fa21_remote/Switch_Control/Switch_Control.ino
@@ -1,17 +1,14 @@
-#define REV "C"      // Revision goes here
-
 #include <Wire.h>
 uint8_t drive = P2_4;       // Control output for the drive switch
 uint8_t clean = P2_5;       // Control output for the clean switch
 float vcc = 3.3;
-uint16_t dac_bits = 4095;
+uint8_t dac_bits = 255;
 
-#define MCP472X_I2CADDR (0x60)
-#define MCP472X_CMD_WRITEDAC (0x40)    // Writes data to the DAC
-#define MCP472X_CMD_WRITEDACEEPROM (0x60) // Writes data to EEPROM
+#define DAC5571_I2CADDR (0x4D)
 
 String buff(20);
-int f = 10;     // Switching Interval
+
+int f = 10;
 
 /* The setup() function runs every time the Arduino is powered
    on or the RESET button is pressed.*/
@@ -20,7 +17,7 @@ void setup() {
   Serial.begin(115200);
   Wire.begin();
 
-  Serial.println("\n+-----Starting Capacitive Touchscreen Switch Control-------+");
+  Serial.println("+-----Starting Capacitive Touchscreen Switch Control-------+");
   Serial.println("| Enter a voltage value between 0V and 3.3V to change Vref |");
   Serial.println("|              The default Vref value is 0.5V              |");
   Serial.println("+----------------------------------------------------------+");
@@ -31,7 +28,7 @@ void setup() {
   digitalWrite(drive, LOW);
   digitalWrite(clean, LOW);
 
-  write_dac(620);
+  write_dac(38);
 }
 
 /* The loop() function is executed over and over as long as the
@@ -40,29 +37,20 @@ void loop () {
   sense();
 }
 
-void write_dac( uint16_t voltage ) {
-  /* Developer's Note (Seiya):
-     Revision version can be found on the top right of the board
-     Rev B is Summer 2020, Rev C is Fall 2020
-     Su20 used MCP4725, Fa20 uses MCP4726A
-     Change "REV" variable to indicate version */
-  Wire.beginTransmission(MCP472X_I2CADDR);
-  if (REV == "B") {
-    Wire.write(MCP472X_CMD_WRITEDAC);
-    Wire.write((uint8_t) (voltage >> 4));
-    Wire.write((uint8_t) (voltage % 16) << 4);
-  }
-  if (REV == "C") {
-    Wire.write((uint8_t) ((voltage >> 8) & 0x0F));
-    Wire.write((uint8_t) (voltage));
-  }
+void write_dac( uint8_t voltage ) {
+  uint8_t packet[2];
+  packet[0] = voltage >> 4;
+  packet[1] = voltage << 4;
+  Wire.beginTransmission(DAC5571_I2CADDR);
+  Wire.write(packet[0]);
+  Wire.write(packet[1]);
   Wire.endTransmission();
 }
 
 void serialEvent() {
   buff = Serial.readStringUntil('\n');
   float buff_int = buff.toFloat();
-  uint16_t to_write = (0x0FFF) & int((buff_int / vcc * dac_bits));
+  uint8_t to_write = (0xFF) & int((buff_int / vcc * dac_bits));
   if (buff_int != 0 && buff_int <= vcc && buff_int >= 0) {
     write_dac(to_write);
     Serial.print("Setting DAC to ");
@@ -86,7 +74,7 @@ void sense() {
 
   // STEP 2: Stop discharging
   digitalWrite(clean, LOW);
-
+  
   // STEP 3: Charge Ctouchscreen
   digitalWrite(drive, HIGH);
 
